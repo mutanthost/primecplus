c346527155ba8a84d9bbf204d2eb3c16506822e1
Matthew Huston <matt@filth.home>
2012-12-30 18:27:14 -0500

    Initial Commit

---
+++ primecplus/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  primecplus
+//
+//  Created by Matthew Huston on 12/30/12.
+//  Copyright (c) 2012 Matthew Huston. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
